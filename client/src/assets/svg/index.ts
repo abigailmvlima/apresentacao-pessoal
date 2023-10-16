9c57a65691c91d79412b20af7d126bc51c7a105f
abigail.lima <macstudio@local>
2023-10-15 21:53:26 -0300

    builds workflow

--- client/src/assets/svg/index.ts
+++ client/src/assets/svg/index.ts
@@ -1,5 +1,5 @@
 import { ReactComponent as LogoHeader } from './biaSilva.svg';
-import { ReactComponent as BSProfissional } from './bsProfissional.svg';
+import { ReactComponent as BSProfissional } from './BSProfissional.svg';
 import { ReactComponent as ChevronRight } from './chevronRight.svg';
 import { ReactComponent as Hello } from './hello.svg';
 import { ReactComponent as IconEdit } from './iconEdit.svg';
@@ -14,8 +14,8 @@ import { ReactComponent as TitleFullStack } from './titleFullStack.svg';
 import { ReactComponent as UpDate } from './upDate.svg';
 
 import { ReactComponent as Facebook } from './facebook.svg';
-import { ReactComponent as Instagram } from './instagram.svg';
 import { ReactComponent as Linkedin } from './iconLinkedin.svg';
+import { ReactComponent as Instagram } from './instagram.svg';
 
 const svg = {
   Facebook,
